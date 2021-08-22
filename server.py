2f0d414f60d93d23438a06ad199b65e133cd914a
Aviv <67284525+avivk9@users.noreply.github.com>
2021-08-22 15:14:26 +0300

    server code update

---
+++ server.py
@@ -0,0 +1,44 @@
+import socket
+import threading
+
+HEADER = 64
+PORT = 5050  # port number
+SERVER = socket.gethostbyname(socket.gethostname())  # server ipv4 adress
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MSG = "!disc"
+
+
+#creating new socket      family             type
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected")
+
+    connected = True
+    while connected:
+        msg_len = conn.recv(HEADER).decode(FORMAT)
+        msg_len = int(msg_len)
+        msg = conn.recv(msg_length).decode(FORMAT)
+        if msg == DISCONNECT_MSG:
+            connected = False
+
+        print(f"[{addr}] {msg}")
+    
+    conn.close()
+
+
+def start():
+    server.listen() 
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+print("[STARTING] server is starting...")
+start()
+
+
