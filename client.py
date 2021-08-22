66f01f8d124273deff90304cc6ecbffc1456d97f
Aviv <67284525+avivk9@users.noreply.github.com>
2021-08-22 23:35:15 +0300

    tictactoe - problems with sending board

--- client.py
+++ client.py
@@ -7,8 +7,12 @@ FORMAT = 'utf-8'
 DISCONNECT_MSG = "!disc"  # disconnecting message
 ADDR = (SERVER, PORT)
 
+
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
+
+
 
 def send(msg):
     message = msg.encode(FORMAT)
@@ -17,8 +21,53 @@ def send(msg):
     send_len += b' ' * (HEADER - len(send_len))
     client.send(send_len)
     client.send(message)
-    print(client.recv(2048).decode(FORMAT))
 
-send("Hello world!")
+def won(board):
+    if board[0] == board[1] == board[2] != '.' or board[3] == board[4] == board[5] != '.' or board[6] == board[7] == board[8] != '.':
+        return True
+    if board[0] == board[3] == board[6] != '.' or board[1] == board[4] == board[7] != '.' or board[2] == board[5] == board[8] != '.':
+        return True
+    if board[0] == board[4] == board[8] != '.' or board[2] == board[4] == board[6] != '.':
+        return True
+    return False
+    
+
+
+def printBoard(board):
+    print(board[0:3])
+    print(board[3:6])
+    print(board[6:9])
+
+def main():
+    board = "........."
+    print("Connected")
+    flag = True
+    while flag:
+        TYPE = input("Do you wanna be X or O? ")
+        TYPE = TYPE.upper()
+        if TYPE == 'X' or TYPE == 'O':
+            flag = False
+    
+    send(TYPE)
+
+    if TYPE == 'X':
+        printBoard(board)
+        placement = input("enter the index to put X (number 1-9): ")
+        board = board[0:int(placement)] + TYPE + board[int(placement) + 1:9]
+        printBoard(board)
+        send(board)
+    
+    
+    while not won(board):
+        board = client.recv(2048).decode(FORMAT)
+        printBoard(board)
+        placement = input(f"enter the index to put {TYPE} (number 1-9): ")
+        board = board[0:int(placement)] + TYPE + board[int(placement) + 1:9]
+        printBoard(board)
+        send(board)
+    
+    send("won")
+
+main()
 
 
--- server.py
+++ server.py
@@ -7,11 +7,30 @@ SERVER = socket.gethostbyname(socket.gethostname())  # server ipv4 adress
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MSG = "!disc"  # disconnecting message
-
+TYPE = 'O'
 
 #creating new socket      family             type
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
+
+
+def won(board):
+    if board[0] == board[1] == board[2] != '.' or board[3] == board[4] == board[5] != '.' or board[6] == board[7] == board[8] != '.':
+        return True
+    if board[0] == board[3] == board[6] != '.' or board[1] == board[4] == board[7] != '.' or board[2] == board[5] == board[8] != '.':
+        return True
+    if board[0] == board[4] == board[8] != '.' or board[2] == board[4] == board[6] != '.':
+        return True
+    return False
+    
+
+
+def printBoard(board):
+    print(board[0:3])
+    print(board[3:6])
+    print(board[6:9])
+
+
 
 def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected")
@@ -22,13 +41,30 @@ def handle_client(conn, addr):
         if msg_len: 
             msg_len = int(msg_len)
             msg = conn.recv(msg_len).decode(FORMAT)
-            if msg == DISCONNECT_MSG:
-                connected = False
+            if msg == 'X':
+                TYPE = 'O'
+            if msg == 'O':
+                TYPE = 'X'
+            if msg == 'won':
+                print("enemy won :(")
+                conn.close()
+            board = msg
+            printBoard(board)
+            placement = input(f"enter the index to put {TYPE} (number 1-9): ")
+            board = board[0:int(placement)] + TYPE + board[int(placement) + 1:9]
+            printBoard(board)
+            if won(board):
+                print("Yay you won :)")
+                conn.close()
+            conn.send(board.encode(FORMAT))
+            
+        
+            
 
-            print(f"[{addr}] {msg}")
-            conn.send("msg recived".encode(FORMAT))
+
+            
     
-    conn.close()
+    
 
 
 def start():
